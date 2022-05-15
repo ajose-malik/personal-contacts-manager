6c444ede6f28a04ee89a4b990bf0adf81d1f6ced
ajosemalik <ajose.malik@gmail.com>
2022-05-15 19:40:29 -0400

    implement Record and Records structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Record {
+    id: i64,
+    name: String,
+    email: Option<String>,
+}
+
+#[derive(Debug)]
+struct Records {
+    inner: HashMap<i64, Record>,
+}
+
+impl Records {
+    fn new() -> Self {
+      self { 
+          inner: HashMap::new(),
+        }
+    }
+
+    fn add(&mut self, record: Record) {
+        self.inner.insert(record.id, record)
+    }
+}
+
+
+fn main () {
+    
 }
